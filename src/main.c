a29d56a8807e4f38164b3dd11cd71bd1406190b5
Dmitry Ponyatov <dponyatov@gmail.com>
2015-12-16 10:56:56 +0400

    Task List works (show only)

--- src/main.c
+++ src/main.c
@@ -1,51 +1,55 @@
 #include <pebble.h>
 #include "main.h"
 
-struct { int tick; char name[]; } TaskPool [] = {
-	{0,"SHED"},
-	{0,"CNC"},
-  {0,"SS"}
-};
-int szTaskPool = sizeof(TaskPool)/sizeof(TaskPool[0]);
-
 #define SCR_W 144
 #define SCR_H 168
+#define SCR_LINES 8
+#define TXT_H SCR_H/SCR_LINES
 
-TextLayer *text_layer;
+char txtDateTime[] = "___00 Mmm Ddd MM:SS___";
+#define TM_FORMAT "___%d %b %a %M:%S___"
+#define TSZ sizeof(txtDateTime)
 
-char txtDateTime[] = "00 Mmm Ddd HH:MM:SS";
-char Scr[80*25];
+struct { int tick; char name[TSZ]; TextLayer *tl; char ts[TSZ]; } TaskPool [] = {
+  { .name = "SHED" }, { .name = "bI script" },
+  { .name = "pss" }, { .name = "tea" },
+  { .name = "CNC" }
+};
+int szTaskPool = sizeof(TaskPool)/sizeof(TaskPool[0]);
+//int active = 0;
 
-char tsk[]="00000000000000000000000";
-char *task(int n) {
-  snprintf(tsk+0,4,"%i",TaskPool[n].tick);
-  snprintf(tsk+4,sizeof(tsk)-4,"%s",TaskPool[n].name);
-  return tsk;
-}
+TextLayer *tlDate;
 
 void update() {
-  time_t now = time(NULL);
-  struct tm *current_time = localtime(&now);
-  strftime(txtDateTime, sizeof(txtDateTime), "%d %b %a %H:%M:%S", current_time);
-  for (int i=0;i<szTaskPool;i++) TaskPool[i].tick++;
-  snprintf(Scr,sizeof(Scr),"= %s = %s %s %s",txtDateTime,task(0),task(1),task(2));
-  text_layer_set_text(text_layer, Scr);
+  // date&time
+  time_t now = time(NULL); struct tm *current_time = localtime(&now);
+  strftime(txtDateTime, sizeof(txtDateTime), TM_FORMAT, current_time);
+  text_layer_set_text(tlDate, txtDateTime);
+  // tasks
+  for (int i=0;i<szTaskPool;i++) {
+    TaskPool[i].tick+=i+1;
+    snprintf(TaskPool[i].ts,TSZ," %i %s",TaskPool[i].tick,TaskPool[i].name);
+    text_layer_set_text(TaskPool[i].tl, TaskPool[i].ts);
+  }
 }
-static void second_tick(struct tm* tick_time, TimeUnits units_changed) {
-  update();
-}
+static void second_tick(struct tm* tick_time, TimeUnits units_changed) { update(); }
 
 int main(void) {
   // main window
   Window *window = window_create(); window_stack_push(window, true);
-  // text layer
-  text_layer = text_layer_create(GRect(0, 0, SCR_W, SCR_H));;
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  // text layers
+  tlDate  = text_layer_create(GRect(0, 0*TXT_H, SCR_W, TXT_H));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(tlDate));
+  for (int i=0;i<szTaskPool;i++) {
+    TaskPool[i].tl = text_layer_create(GRect(0, (i+1)*TXT_H, SCR_W, TXT_H));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(TaskPool[i].tl));
+  }
   // setup timers
   tick_timer_service_subscribe(SECOND_UNIT, &second_tick);
   // apprun
   app_event_loop();
   // cleanup
-  text_layer_destroy(text_layer);
+  text_layer_destroy(tlDate);
+  for (int i=0;i<szTaskPool;i++) text_layer_destroy(TaskPool[i].tl);
   window_destroy(window);
 }
