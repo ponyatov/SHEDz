fd7a46844347169489165b2cdbd472118f8aaa12
Dmitry Ponyatov <dponyatov@gmail.com>
2015-12-18 16:16:49 +0400

    + shed test works
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- shed.py
+++ shed.py
@@ -1,19 +1,27 @@
 pool = [
-    [ 0, 0, 'cnc', 5],
-    [ 29, 1, 'pij', 2],
-    [ 0, 0, 'bi', 0],
-    [ 29, 1, 'vrep', 3],
-    [ 28, 6, 'shed', 0]
+    # slot count name prio
+##    [ 0, 1, 'cnc', 5],
+##    [ 29, 1, 'pij', 2],
+##    [ 0, 1, 'bi', 1],
+##    [ 29, 1, 'vrep', 3],
+##    [ 28, 6, 'shed', 1]
+        [ 0, 3, 'pij', 5],
+        [ 0, 5, 'cnc', 3],
+        [ 0, 2, 'bi', 11],
     ]
 
-def printpool():
+def printpool(pool):
     print 'slot\tcount\tname\tprio\n'+'-'*40
     for i in pool:
         print '%s\t%s\t%s\t%s\t'%(i[0],i[1],i[2],i[3])
     print
 
-def shedule(): pass
+def SF(T):
+    return T[1]*T[3]
 
-printpool()
+def shedule():
+    pool.sort(key=SF)
+
+printpool(pool)
 shedule()
-printpool()
+printpool(pool)
